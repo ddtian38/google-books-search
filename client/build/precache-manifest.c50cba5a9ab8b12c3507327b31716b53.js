20a3e7f4ae61656270c79be6de4b0415473e2fcc
Daniel Tian <danieltian31@yahoo.com>
2019-05-01 08:51:59 -0500

    set up inital files

---
+++ client/build/precache-manifest.c50cba5a9ab8b12c3507327b31716b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "754b3afc7bad899f3311",
+    "url": "/static/js/main.e732c45d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "754b3afc7bad899f3311",
+    "url": "/static/css/main.4b7cf4fd.chunk.css"
+  },
+  {
+    "revision": "cca0b493a6ba0a29912a582e35b9cd81",
+    "url": "/index.html"
+  }
+];
